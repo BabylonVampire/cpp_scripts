1753f4ae59b21d2c6a502fd5750afdc3da527172
Табалов Никита <92277573+BabylonVampire@users.noreply.github.com>
2022-03-21 11:31:26 +0300

    Add files via upload

---
+++ encrypting_program.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void ceasar(string &str, string key) {
+	int offset = stoi(key);
+	int length = str.length();
+
+	for (size_t i = 0; i < length; ++i) {
+		str[i] = (str[i] + offset) % 256;
+	}
+}
+
+void vernam(string &str, string key) {
+	int key_length = key.length();
+	int length = str.length();
+
+	for (size_t i = 0; i < length; ++i) {
+		if (str[i] != ' ') {
+			str[i] = str[i] ^ key[i % key_length];
+		}
+	}
+}
+
+void alphabet_reverse(string &str, string key) {
+	int length = str.length();
+
+	for (size_t i = 0; i < length; ++i) {
+		if (str[i] != ' ') {
+			str[i] = 255 - str[i];
+		}
+	}
+}
+
+void decrypt_ceasar(string &str, string key) {
+	int length = str.length();
+	int offset = stoi(key);
+
+	for (size_t i = 0; i < length; ++i) {
+		if (str[i] - offset < 0) str[i] = str[i] - offset + 255;
+		else str[i] = (str[i] - offset) % 255;
+	}
+}
+
+void decrypt_vernam(string &str, string key) {
+	int key_length = key.length();
+	int length = str.length();
+
+	for (size_t i = 0; i < length; ++i) {
+		if (str[i] != ' ') {
+			str[i] = str[i] ^ key[i % key_length];
+		}
+	}
+}
+
+void encrypt(string combination, void(**cipher_funks)(string&, string), string name_of_file) {
+	int index, length;
+	bool writing_key = false, first_stroke = true;
+	ifstream target_file;
+	ofstream new_file;
+	string lockal_key, text_of_stroke, key, public_key = "";
+	length = combination.length();
+	string* keys = new string[length];
+
+	target_file.open(name_of_file + ".txt");
+	new_file.open(name_of_file + "_encrypted.txt");
+
+	while (getline(target_file, text_of_stroke)) {
+		if (first_stroke) {
+			for (size_t i = 0; i < length; ++i) {
+				key = "";
+				index = combination[i] - '0';
+				switch (index) {
+				case 1:
+					cout << "[+] Enter the size of offset: ";
+					cin >> key;
+					keys[i] = key;
+					break;
+				case 2:
+					cout << "[+] Enter secret key for vernam cipher: ";
+					cin >> key;
+					keys[i] = key;
+					break;
+				case 3:
+					cout << "[+] Alphabet has been reversed." << endl;
+					break;
+				default:
+					cout << "[-] Error: Could not find function!" << endl;
+					system("pause");
+					exit(1);
+				}
+				cipher_funks[index - 1](text_of_stroke, key);
+				public_key += to_string(index - 1) + "(" + key + ")";
+				first_stroke = false;
+			}
+		}
+		else {
+			for (size_t i = 0; i < length; ++i) {
+				index = combination[i] - '0';
+				key = keys[i];
+				if (index > 4) {
+					cout << "[-] Error: Could not find function!" << endl;
+					system("pause");
+					exit(1);
+				}
+				cipher_funks[index - 1](text_of_stroke, key);
+			}
+		}
+		new_file << text_of_stroke << "\n";
+	}
+
+	cout << "[+] Your key-code is: " + public_key << endl;
+
+	delete[] keys;
+	new_file.close();
+}
+
+void decrypt(string combination, void(**decipher_funks)(string&, string), string name_of_file) {
+	string lockal_key = "", text_of_stroke;
+	bool writing_key = false;
+	int length = combination.length();
+	ifstream target_file;
+	ofstream new_file;
+	vector<string> keys;
+	vector<int> funks;
+
+	for (size_t i = 0; i < length; ++i) {
+		if (combination[i] == '(') {
+			writing_key = true;
+		}
+		else if (combination[i] == ')') {
+			writing_key = false;
+			keys.push_back(lockal_key);
+			lockal_key = "";
+		}
+		else if (writing_key) lockal_key += combination[i];
+		else funks.push_back(combination[i]);
+	}
+
+	target_file.open(name_of_file + ".txt");
+	new_file.open(name_of_file + "_decrypted.txt");
+	length = funks.size();
+
+	while (getline(target_file, text_of_stroke)) {
+		for (int i = 0; i < length; ++i) {
+			decipher_funks[funks[length - i - 1] - '0'](text_of_stroke, keys[length - i - 1]);
+		}
+		new_file << text_of_stroke + "\n";
+	}
+}
+
+void information() {
+	cout << "Welcome to the text cipherer!\nHere you can protect your information and decrypt one, that alredy encrypted!\nHave fun :)" << endl;
+	cout << "1) Caesar cipher" << endl;
+	cout << "2) Vernam cipher" << endl;
+	cout << "3) reverse alphabet" << endl;
+	cout << "Commands:" << endl;
+	cout << "1. 'encrypt' and combination of numbers of ciphers to encrypt text." << endl;
+	cout << "2. 'decrypt' and combination of numbers of ciphers to decrypt text." << endl;
+}
+
+void main() {
+	string input_command, name_of_file, input_combination = "";
+
+	information();
+
+	cout << "[+] Enter name of text file, you want to encrypt/decrypt: ";
+	cin >> name_of_file;
+
+	cout << "[+] command: ";
+	cin >> input_command >> input_combination;
+
+	void(**cipher_funks)(string&, string) = new (void(*[3])(string&, string));
+	cipher_funks[0] = ceasar;
+	cipher_funks[1] = vernam;
+	cipher_funks[2] = alphabet_reverse;
+
+	void(**decrypt_funks)(string&, string) = new (void(*[3])(string&, string));
+	decrypt_funks[0] = decrypt_ceasar;
+	decrypt_funks[1] = decrypt_vernam;
+	decrypt_funks[2] = alphabet_reverse;
+
+	if (input_command == "encrypt" && input_command != "") {
+		encrypt(input_combination, cipher_funks, name_of_file);
+	}
+	else if (input_command == "decrypt" && input_command != "") {
+		decrypt(input_combination, decrypt_funks, name_of_file);
+	}
+	else {
+		cout << "[-] Please, enter correct command!" << endl;
+		system("pause");
+		exit(1);
+	}
+
+	cout << "[+] Programm finished succesfuly!" << endl;
+
+	delete[] cipher_funks;
+	delete[] decrypt_funks;
+}
---
+++ life_game.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+#include <windows.h>
+
+using namespace std;
+
+void print(int x, int y, char** field) {
+	//Функция для вывода игрового поля
+	for (size_t i = 1; i < x - 1; ++i) {
+		for (size_t j = 1; j < y - 1; ++j) {
+			cout << field[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void behaveour(int x, int y, char** field, int &counter) {
+	//функуция по подсчету 'живых' клеток по соседству с данной
+	for (size_t i = x - 1; i <= x + 1; ++i) {
+		for (size_t j = y - 1; j <= y + 1; ++j) {
+			if (field[i][j] == '*' && (i != x || j != y)) ++counter;
+		}
+	}
+}
+
+void birth_and_dying(int x, int y, char** field, int &counter_dying, int& counter_birth, vector<array <int, 2>>& births, vector<array <int, 2>>& dyings) {
+	//фуинкция, определяющая и заносящая в векторы координаты точек, которые должны родится/умереть
+	int counter = 0;
+	array<int, 2> dot;
+	for (size_t i = 1; i < x - 1; ++i) {
+		for (size_t j = 1; j < y - 1; ++j) {
+			behaveour(i, j, field, counter);
+			if ((counter < 2 || counter > 3) && field[i][j] == '*') {
+				dot.at(0) = i;
+				dot.at(1) = j;
+				dyings.push_back(dot);
+				++counter_dying;
+			}
+			else if (counter == 3 && field[i][j] != '*') {
+				dot.at(0) = i;
+				dot.at(1) = j;
+				births.push_back(dot);
+				++counter_birth;
+			}
+			counter = 0;
+		}
+	}
+}
+
+void killing_and_reprodusing(vector<array <int, 2>>& births, vector<array <int, 2>>& dyings, char** field) {
+	//функиця, обновляющая поле, исходя из данных о тех, кто умер/родился
+	//вынесена в отдельную по причине того, что программе важно сначала понять, кто умрет/родится, а уже затем использовать эти данные, чтобы все происходило одновременно
+	for (size_t i = 0; i < births.size(); ++i) {
+		field[births.at(i)[0]][births.at(i)[1]] = '*';
+	}
+	for (size_t i = 0; i < dyings.size(); ++i) {
+		field[dyings.at(i)[0]][dyings.at(i)[1]] = ' ';
+	}
+}
+
+void statistics(int day, int counter_dying, int counter_birth) {
+	cout << "day: " << day << endl;
+	cout << "Number of births: " << counter_birth << endl;
+	cout << "Number of deaths: " << counter_dying << endl;
+}
+
+int main() {
+	int x, y;
+	cout << "Enter wight and height of field (natural numbers): ";
+	cin >> x >> y;
+	x += 2;
+	y += 2;
+	char** field = new char* [x];
+	for (size_t i = 0; i < x; ++i) {
+		field[i] = new char[y];
+		for (int j = 0; j < y; ++j) {
+			field[i][j] = ' ';
+		}
+	}
+	cout << "msg: Enter only natural numbers!" << endl;
+	cout << "msg: Enter \"-1 -1\" to end" << endl;
+	int a, b, counter = 1;
+	do {
+		cout << "[" << counter << "]: ";
+		cin >> a >> b;
+		if (a != -1 || b != -1) {
+			field[a + 1][b + 1] = '*';
+		}
+		++counter;
+	} while (a != -1 && b != -1);
+	int counter_birth = 0, counter_dying = 0, total_births = 0, total_deaths = 0;
+	counter = 1;
+	vector <array <int, 2>> births;
+	vector <array <int, 2>> deaths;
+	while (true) {
+		system("cls");
+		print(x, y, field);
+		birth_and_dying(x, y, field, counter_birth, counter_dying, births, deaths);
+		killing_and_reprodusing(births, deaths, field);
+		total_births += counter_birth;
+		total_deaths += counter_dying;
+		statistics(counter, total_deaths, total_births);
+		births.clear();
+		deaths.clear();
+		++counter;
+		if (counter_birth == 0 && counter_dying == 0) {
+			cout << "msg: The game is over!" << endl;
+			system("pause");
+			for (size_t i = 0; i < x; ++i) delete field[i];
+			delete[] field;
+			exit(1);
+		}
+		counter_birth = 0;
+		counter_dying = 0;
+		system("pause");
+	}
+}
